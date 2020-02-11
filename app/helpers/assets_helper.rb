420f2fa2d5276ee6cfa991f530e04d76aa710ffe
KenyaSogo <kenyasogo.dev@gmail.com>
2020-02-11 05:43:00 +0000

    add aggregate_datetime field to asset index page

--- app/helpers/assets_helper.rb
+++ app/helpers/assets_helper.rb
@@ -1,2 +1,6 @@
 module AssetsHelper
+  def last_aggregated_at(asset)
+    aggregated_at = asset.last_aggregate_succeeded_at
+    aggregated_at.blank? ? '-' : aggregated_at.strftime('%Y/%m/%d %H:%M:%S')
+  end
 end

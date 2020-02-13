ab4b1105fa0d673ac2aa91328894594daec40bb6
KenyaSogo <kenyasogo.dev@gmail.com>
2020-02-13 00:37:52 +0000

    add search form to asset_activities index page

--- app/controllers/asset_activities_controller.rb
+++ app/controllers/asset_activities_controller.rb
@@ -6,10 +6,12 @@ class AssetActivitiesController < ApplicationController
   # GET /asset_activities
   # GET /asset_activities.json
   def index
-    if params[:asset_id].present?
-      asset = Asset.find(params[:asset_id])
-      current_users_resource_filter(asset)
-      @asset_activities = AssetActivity.where(asset_account_id: asset.asset_accounts.map(&:id)).page(params[:page]).per(30).order(transaction_date: :desc)
+    asset_id_param = params[:q].present? ? params[:q][:asset_id] : params[:asset_id]
+    if asset_id_param.present?
+      @asset = Asset.find(asset_id_param)
+      current_users_resource_filter(@asset)
+      @query = AssetActivity.where(asset_account_id: @asset.asset_accounts.map(&:id)).ransack(params[:q])
+      @asset_activities = @query.result.page(params[:page]).per(30).order(transaction_date: :desc)
     end
   end
 

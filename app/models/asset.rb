853ebd6f4c23c696514830ab6ee7bc40b4367bd4
KenyaSogo <kenyasogo.dev@gmail.com>
2020-02-19 14:29:47 +0000

    add simulations table to asset show page

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,5 +1,6 @@
 class Asset < ApplicationRecord
   belongs_to :user
   has_many :asset_accounts, dependent: :destroy
+  has_many :simulations, dependent: :destroy
   has_many :items, dependent: :destroy
 end

ad7c183c1649cf7fb782354fa0544f59598e39d7
KenyaSogo <kenyasogo.dev@gmail.com>
2020-02-19 16:24:58 +0000

    add simulation_entries table to simulation show page

--- app/models/simulation.rb
+++ app/models/simulation.rb
@@ -1,3 +1,4 @@
 class Simulation < ApplicationRecord
   belongs_to :asset
+  has_many :simulation_entries, dependent: :destroy
 end
--- app/models/simulation_entry.rb
+++ app/models/simulation_entry.rb
@@ -1,4 +1,5 @@
 class SimulationEntry < ApplicationRecord
   belongs_to :simulation
   belongs_to :simulation_entry_type
+  has_many :simulation_entry_details, dependent: :destroy
 end

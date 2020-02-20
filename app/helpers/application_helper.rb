b352d785d02794449c53908a7c22ff1bbd0298a5
KenyaSogo <kenyasogo.dev@gmail.com>
2020-02-20 01:51:29 +0000

    fix: refactor helper method: display_datetime_string

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,7 @@ module ApplicationHelper
   def user_display_name(user)
     user.user_name.blank? ? user.email : user.user_name
   end
+  def display_datetime_string(datetime)
+    datetime.blank? ? '-' : datetime.strftime('%Y/%m/%d %H:%M:%S')
+  end
 end
--- app/helpers/assets_helper.rb
+++ app/helpers/assets_helper.rb
@@ -1,5 +1,2 @@
 module AssetsHelper
-  def display_date_string(datetime)
-    datetime.blank? ? '-' : datetime.strftime('%Y/%m/%d %H:%M:%S')
-  end
 end

ed6744698619d024f6d7ecab4a4698903ad09a61
KenyaSogo <kenyasogo.dev@gmail.com>
2020-02-23 12:02:11 +0000

    add validation to simulation_entry

--- app/models/simulation_entry.rb
+++ app/models/simulation_entry.rb
@@ -2,4 +2,21 @@ class SimulationEntry < ApplicationRecord
   belongs_to :simulation
   belongs_to :simulation_entry_type
   has_many :simulation_entry_details, dependent: :destroy
+
+  validates :name, :simulation_entry_type_id, :apply_from, :apply_to, presence: true
+  validates :name, uniqueness: { scope: :simulation }
+  validate :validate_apply_from_to_range
+  validate :validate_entry_type_change, on: :update
+
+  private
+
+  def validate_apply_from_to_range
+    return if apply_from.blank? || apply_to.blank?
+    errors.add(:apply_to, 'must be greater than or equal to apply from') unless apply_to >= apply_from
+    errors.add(:apply_to, 'and from range must be less than or equal to 5 years') unless apply_to <= apply_from.next_year(5)
+  end
+
+  def validate_entry_type_change
+    errors.add(:simulation_entry_type_id, 'is unable to change') if simulation_entry_type_id_changed?
+  end
 end

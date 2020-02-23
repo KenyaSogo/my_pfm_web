d5b8c0244f7c89c259cc4e4ea1f69a3dd1ab55d6
KenyaSogo <kenyasogo.dev@gmail.com>
2020-02-23 10:42:29 +0000

    add validation to simulation

--- app/models/simulation.rb
+++ app/models/simulation.rb
@@ -1,4 +1,6 @@
 class Simulation < ApplicationRecord
   belongs_to :asset
   has_many :simulation_entries, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: { scope: :asset }
 end

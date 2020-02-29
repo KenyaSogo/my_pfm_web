31f2d5e025c38f19a47ed0cdafea8233449f606b
KenyaSogo <kenyasogo.dev@gmail.com>
2020-02-29 07:51:25 +0000

    add billing_accounts table to simulation show page

--- app/models/simulation.rb
+++ app/models/simulation.rb
@@ -1,6 +1,7 @@
 class Simulation < ApplicationRecord
   belongs_to :asset
   has_many :simulation_entries, dependent: :destroy
+  has_many :billing_accounts, dependent: :destroy
 
   validates :name, presence: true, uniqueness: { scope: :asset }
 end

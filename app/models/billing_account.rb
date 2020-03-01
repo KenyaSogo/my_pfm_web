5c561a37d59c01f98c95d9180cdcb90a9c6f88b3
KenyaSogo <kenyasogo.dev@gmail.com>
2020-03-01 07:35:57 +0000

    add class_name to billing_account columns

--- app/models/billing_account.rb
+++ app/models/billing_account.rb
@@ -1,9 +1,9 @@
 class BillingAccount < ApplicationRecord
   belongs_to :simulation
-  belongs_to :credit_account
-  belongs_to :billing_item
-  belongs_to :billing_sub_item
-  belongs_to :debit_account
-  belongs_to :debit_item
-  belongs_to :debit_sub_item
+  belongs_to :credit_account, class_name: 'AssetAccount'
+  belongs_to :billing_item, class_name: 'Item'
+  belongs_to :billing_sub_item, class_name: 'SubItem'
+  belongs_to :debit_account, class_name: 'AssetAccount'
+  belongs_to :debit_item, class_name: 'Item'
+  belongs_to :debit_sub_item, class_name: 'SubItem'
 end

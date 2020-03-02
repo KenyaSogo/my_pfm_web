0defc85bd85e4fe1be22e6a8cd1c721fb0092b93
KenyaSogo <kenyasogo.dev@gmail.com>
2020-03-02 14:13:16 +0000

    add billing_counts to simulations table at assets show page

--- app/helpers/assets_helper.rb
+++ app/helpers/assets_helper.rb
@@ -1,2 +1,8 @@
 module AssetsHelper
+  def result_activity_counts(simulation)
+    simulation_result_activity_counts = simulation.simulation_entries.joins(simulation_entry_details: :simulation_result_activities).size
+    billing_activity_counts = simulation.billing_accounts.joins(:billing_activities).size
+
+    simulation_result_activity_counts + billing_activity_counts
+  end
 end

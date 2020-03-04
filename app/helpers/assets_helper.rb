bcd7c39d80a1e396fa349da89da90f95d1142ad8
KenyaSogo <kenyasogo.dev@gmail.com>
2020-03-04 08:09:08 +0000

    add billing_account releated counts solumn to simulations table at asset show page

--- app/helpers/assets_helper.rb
+++ app/helpers/assets_helper.rb
@@ -1,9 +1,12 @@
 module AssetsHelper
   def result_activity_counts(simulation)
-    simulation_result_activity_counts = simulation.simulation_entries.joins(simulation_entry_details: :simulation_result_activities).size
-    billing_activity_counts = simulation.billing_accounts.joins(:billing_activities).size
-    billing_activity_counts += simulation.billing_accounts.joins(:billing_complement_activities).size
+    simulation.simulation_entries.joins(simulation_entry_details: :simulation_result_activities).size
+  end
 
-    simulation_result_activity_counts + billing_activity_counts
+  def billing_activity_counts(simulation)
+    billing_activity_counts = simulation.billing_accounts.joins(:billing_activities).size
+    billing_complement_activity_counts = simulation.billing_accounts.joins(:billing_complement_activities).size
+
+    billing_activity_counts + billing_complement_activity_counts
   end
 end

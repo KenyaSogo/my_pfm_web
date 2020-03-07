d9b2f7b7b64f571ffebcedbb50a5507ceb8ad99b
KenyaSogo <kenyasogo.dev@gmail.com>
2020-03-07 08:19:44 +0000

    add simulation_summary_breakdowns table to simulation_summary show page

--- app/helpers/simulation_summaries_helper.rb
+++ app/helpers/simulation_summaries_helper.rb
@@ -1,2 +1,10 @@
 module SimulationSummariesHelper
+  def simulation_summary_breakdown_type_name(breakdown)
+    case
+    when breakdown.is_a?(SimulationSummaryByAccount)
+      'ByAccount'
+    else
+      '-'
+    end
+  end
 end

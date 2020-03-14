51f6bfc620b361414a606c86f7906b4b51bd61c6
KenyaSogo <kenyasogo.dev@gmail.com>
2020-03-14 02:14:57 +0000

    simulation_summary_by_account: set default is_active: true

--- app/models/simulation_summary_by_account.rb
+++ app/models/simulation_summary_by_account.rb
@@ -2,10 +2,16 @@ class SimulationSummaryByAccount < ApplicationRecord
   belongs_to :simulation_summary
   has_many :sum_account_dailies, dependent: :destroy
 
+  before_validation :set_is_active_true, on: :create
+
   validates :memo, length: { maximum: 400 }
   validate :validate_unity_within_simulation_summary, on: :create
 
   private
+
+  def set_is_active_true
+    self.is_active = true
+  end
 
   def validate_unity_within_simulation_summary
     return if SimulationSummaryByAccount.where(simulation_summary_id: simulation_summary.id).size == 0

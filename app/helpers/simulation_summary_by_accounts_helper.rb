85c470c4bf838e9ddec325c6cb0db4f4f3258bb1
KenyaSogo <kenyasogo.dev@gmail.com>
2020-03-15 04:41:59 +0000

    fix: method_name to unique: asset_account_options

--- app/helpers/simulation_summary_by_accounts_helper.rb
+++ app/helpers/simulation_summary_by_accounts_helper.rb
@@ -1,5 +1,5 @@
 module SimulationSummaryByAccountsHelper
-  def asset_account_options(simulation_summary_by_account)
+  def summary_by_asset_account_options(simulation_summary_by_account)
     asset_account_ids = simulation_summary_by_account.sum_account_dailies.select(:asset_account_id).distinct.pluck(:asset_account_id)
     AssetAccount.where(id: asset_account_ids).map { |asset_account| [asset_account.name, asset_account.id] }.to_h
   end

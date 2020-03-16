87c6d0c3973dff7ed53dc513b5050d1b90299bec
KenyaSogo <kenyasogo.dev@gmail.com>
2020-03-16 14:53:40 +0000

    add validation to sum_acct_class_daily

--- app/models/sum_acct_class_daily.rb
+++ app/models/sum_acct_class_daily.rb
@@ -1,4 +1,6 @@
 class SumAcctClassDaily < ApplicationRecord
   belongs_to :sum_by_account_class
   belongs_to :simulation_acct_class
+
+  validates :sum_by_account_class, :base_date, :simulation_acct_class, :balance, presence: true
 end

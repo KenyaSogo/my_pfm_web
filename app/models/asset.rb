e3ef723c4d7833a9a11288a4d2fdfc47c317f9b3
KenyaSogo <kenyasogo.dev@gmail.com>
2020-03-22 10:10:07 +0000

    fix: destroy order: consider foreign key restriction

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,7 +1,7 @@
 class Asset < ApplicationRecord
   belongs_to :user
-  has_many :asset_accounts, dependent: :destroy
   has_many :simulations, dependent: :destroy
+  has_many :asset_accounts, dependent: :destroy
   has_many :items, dependent: :destroy
 
   validates :name, presence: true, uniqueness: { scope: :user }

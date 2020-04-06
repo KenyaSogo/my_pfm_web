d29fc7f618137d81f1bb890c940d5d4dd6c10dcc
KenyaSogo <kenyasogo.dev@gmail.com>
2020-04-06 13:37:53 +0000

    fix: consider asset_account add case at sum_by_account_class_setting show page

--- app/controllers/sum_by_acct_class_settings_controller.rb
+++ app/controllers/sum_by_acct_class_settings_controller.rb
@@ -6,6 +6,12 @@ class SumByAcctClassSettingsController < ApplicationController
   # GET /sum_by_acct_class_settings/1
   # GET /sum_by_acct_class_settings/1.json
   def show
+    @asset_accounts = @sum_by_acct_class_setting.sum_by_account_class.simulation_summary.simulation.asset.asset_accounts
+    @acct_to_class_maps = @sum_by_acct_class_setting.acct_to_class_maps
+
+    (@asset_accounts.pluck(:id) - @acct_to_class_maps.pluck(:asset_account_id)).each do |asset_account_id|
+      AcctToClassMap.create!(sum_by_acct_class_setting_id: @sum_by_acct_class_setting.id, asset_account_id: asset_account_id)
+    end
   end
 
   private

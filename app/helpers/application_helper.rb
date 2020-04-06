80a614d91019be42fcd62c5dff0bc935b12c3ead
KenyaSogo <kenyasogo.dev@gmail.com>
2020-04-06 09:06:31 +0000

    cosme: application_helper: add space

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,19 +2,24 @@ module ApplicationHelper
   def user_display_name(user)
     user.user_name.blank? ? user.email : user.user_name
   end
+
   def display_datetime_string(datetime)
     datetime.blank? ? '-' : datetime.strftime('%Y/%m/%d %H:%M:%S')
   end
+
   def display_date_string(date)
     date.blank? ? '-' : date.strftime('%Y/%m/%d')
   end
+
   def year_options
     current_year = Time.current.year
     Range.new(current_year - 5, current_year + 5).map { |y| [y, y] }
   end
+
   def month_options
     Range.new(1, 12).map { |m| [m, m] }
   end
+
   def weekday_options
     {
       Sun: 1,
@@ -26,12 +31,15 @@ module ApplicationHelper
       Sat: 7,
     }
   end
+
   def weekday_name(value)
     weekday_options.key(value)
   end
+
   def day_options
     Range.new(1, 31).map { |d| [d, d] }
   end
+
   def nl2br(str)
     return str if str.blank?
     raw(h(str).gsub(/\R/, "<br />"))

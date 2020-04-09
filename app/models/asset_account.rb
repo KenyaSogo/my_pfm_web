88e6ac92a99467c3ffc1b30c7e0483aaf87f686f
KenyaSogo <kenyasogo.dev@gmail.com>
2020-04-09 00:40:43 +0000

    asset_account validation: abort validation if blank field exists

--- app/models/asset_account.rb
+++ app/models/asset_account.rb
@@ -4,10 +4,15 @@ class AssetAccount < ApplicationRecord
   has_many :asset_activities, dependent: :destroy
 
   validates :asset_type, :initial_balance, :initial_balance_base_date, presence: true, on: :update
+  validate :abort_validation_if_errors_present, on: :update
   validates :initial_balance, numericality: { only_integer: true }, on: :update
   validate :validate_initial_balance_base_date_range, on: :update
 
   private
+
+  def abort_validation_if_errors_present
+    throw :abort if errors.present?
+  end
 
   def validate_initial_balance_base_date_range
     errors.add(:initial_balance_base_date, 'must be past date (or today)') if initial_balance_base_date > Date.today

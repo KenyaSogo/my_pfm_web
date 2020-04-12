789cee87328b95494fc20b4e79fe7bc150344e28
KenyaSogo <kenyasogo.dev@gmail.com>
2020-04-12 04:26:36 +0000

    fix: consider simulation_acct_class validation when sum_by_account_class destroy

--- app/models/sum_by_account_class.rb
+++ app/models/sum_by_account_class.rb
@@ -2,6 +2,7 @@ class SumByAccountClass < ApplicationRecord
   belongs_to :simulation_summary
 
   before_destroy :abort_destroy_if_mapped_to_simulation_summary
+  before_destroy :clear_acct_to_class_maps
 
   has_many :sum_acct_class_dailies, dependent: :destroy
   has_one :sum_by_acct_class_setting, dependent: :destroy
@@ -12,6 +13,10 @@ class SumByAccountClass < ApplicationRecord
   after_create :create_sum_by_acct_class_setting
 
   private
+
+  def clear_acct_to_class_maps
+    sum_by_acct_class_setting.acct_to_class_maps.each { |m| m.update!(simulation_acct_class: nil) }
+  end
 
   def abort_destroy_if_mapped_to_simulation_summary
     if simulation_summary.main_breakdown_type_id == 3 && simulation_summary.main_breakdown_id == id
